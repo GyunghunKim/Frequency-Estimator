0ef5e7414df0adb58f12aa5c3e676c6ab761014d
Gyunghun Kim <ghuni@snu.ac.kr>
2021-08-23 20:31:34 +0900

    Jupyter notebook implementation of Cramer Rao Bound Calculator

--- cramer_rao_bound.py
+++ cramer_rao_bound.py
@@ -4,22 +4,20 @@
 # 3. 이것도 엄청난 수준의 근사이므로 그럴듯한 논거가 될 것이다. (fringe 때문에
 #    가정 2의 성립이 사실상 불가능함)
 
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import *
+import scipy.signal, scipy.interpolate
 
-fig = plt.figure()
 
 # Parameters
+alpha = 0.25
 beta = 0.67
 f = 100e6
-alpha = 0.25
 D = 11.5e3 ** 2 / 1e-6
 T = 50e-6
 
 
-# Plot 1 (Information vs time)
 def information0(t):
     return (beta*2*np.pi*t*np.sin(2*np.pi*f*t))**2/(1-(alpha+beta*np.cos(2*np.pi*f*t))**2)
 
@@ -45,16 +43,38 @@ def information(t, n=0):
     # return prob(t, n, 1, f)
     return sum([1/prob(t, n, r, f)*(dprob(t, n, r, f))**2 for r in [-1, 1]])
 
-t_list = np.linspace(1e-9, 100e-9, 100)
+def upper_envelope(lst):
+    x = []
+    y = []
+    for i, data in enumerate(lst):
+        if 0 < i < len(lst)-1:
+            if lst[i-1] < data and lst[i+1] < data:
+                x.append(i)
+                y.append(data)
 
+    func_y = scipy.interpolate.interp1d(x, y, fill_value='extrapolate')
+
+    envelope = []
+    for i in range(len(lst)):
+         envelope.append(func_y(i))
+
+    return envelope
+
+###########################################################
+# Figure 1
+###########################################################
+
+fig = plt.figure()
+
+t_list = np.linspace(1e-9, 100e-9, 100)
 ax = fig.add_subplot(2, 2, 1)
-ax.set_title("Fisher information\n($f=100\mathrm{MHz}$)")
+ax.set_title("Fisher information (Upper Envelope)\n($f=100\mathrm{MHz}$)")
 for n in (0, 50, 100, 500, 1000):
     if n == 0:
         b_list = [information0(t) for t in t_list]
     else:
         b_list = [information(t, n) for t in t_list]
-    ax.plot(t_list*1e9, b_list, label=f"$n={n}$")
+    ax.plot(t_list*1e9, upper_envelope(b_list), label=f"$n={n}$")
 ax.legend()
 ax.set_xlabel("$t$ [ns]")
 ax.set_ylabel("$\mathcal{I}(t)$")
@@ -70,74 +90,70 @@ for n in (20, 50, 100, 500, 1000):
         b_list = [information0(t) for t in t_list]
     else:
         b_list = [information(t, n) for t in t_list]
-    ax.plot(t_list*1e9, b_list, label=f"$n={n}$")
+    ax.plot(t_list*1e9, upper_envelope(b_list), label=f"$n={n}$")
 ax.legend()
 ax.set_xlabel("$t$ [ns]")
 ax.set_ylabel("$\mathcal{I}(t)$")
 
 
-# Plot 3
-t_list = np.linspace(1e-9, 40e-9, 100)
+# Finding the maximum information
+# t_list = np.linspace(1e-9, 400e-9, 300)
+t_list = np.linspace(1e-9, 10e-6, 300)
 
 ax = fig.add_subplot(2, 2, 3)
+ax.set_title("Fisher information\n($f=100\mathrm{MHz}$)")
+for n in (1, 2, 5, 10):
+    if n == 0:
+        b_list = [information0(t) for t in t_list]
+    else:
+        b_list = [information(t, n) for t in t_list]
+    ax.plot(t_list*1e9, upper_envelope(b_list), label=f"$n={n}$")
+ax.legend()
+ax.set_xlabel("$t$ [ns]")
+ax.set_ylabel("$\mathcal{I}(t)$")
+
+# Plot 4
+t_list = np.linspace(1e-9, 200e-9, 100)
+
+ax = fig.add_subplot(2, 2, 4)
 ax.set_title("Fisher information\n($n=0$)")
-for f in np.linspace(95e6, 105e6, 3):
+for f in (50e6, 95e6, 100e6, 105e6, 150e6):
     b_list = [information0(t) for t in t_list]
-    ax.plot(t_list*1e9, b_list, label=f"$f={f/100e6:.2f}$MHz")
+    ax.plot(t_list*1e9, upper_envelope(b_list), label=f"$f={f/100e6:.2f}$MHz")
 f = 100e6 # Don't forget this line
 ax.legend()
 ax.set_xlabel("$t$ [ns]")
 ax.set_ylabel("$\mathcal{I}(t)$")
 
-# Finding the maximum information
-# t_list = np.linspace(1e-9, 400e-9, 300)
-t_list = np.linspace(1e-9, 3e-6, 300)
+plt.tight_layout()
+plt.show(block=False)
+plt.pause(0.0001)
 
-ax = fig.add_subplot(2, 2, 4)
-ax.set_title("Fisher information\n($f=100\mathrm{MHz}$)")
-for n in (1,2,3,4,5,6,7,8,9,10):
-    if n == 0:
-        b_list = [information0(t) for t in t_list]
-    else:
-        b_list = [information(t, n) for t in t_list]
-    ax.plot(t_list*1e9, b_list, label=f"$n={n}$")
-ax.legend()
-ax.set_xlabel("$t$ [ns]")
-ax.set_ylabel("$\mathcal{I}(t)$")
+###########################################################
+# Figure 2
+###########################################################
 
+fig = plt.figure()
 
-# index_range = range(1, 100)
-# 
-# mse_by_average_list = []
-# for i in index_range:
-#     mse_by_average_list.append(np.sqrt(2 * T * D * i / 2))
-# 
-# ax2 = fig.add_subplot(2, 2, 2)
-# ax2.set_title("MSE by fluctuation")
-# ax2.plot(index_range, mse_by_average_list)
-# ax2.set_xlabel("Number of samples")
-# ax2.set_ylabel("MSE [Hz]")
-# ax2.ticklabel_format(axis="y", style="sci", scilimits=(6, 6), useOffset=False)
+def find_maxinfo_and_t(n):
+    return 0, 0
 
-# mse_list = []
-# mse_using_max_info_list = []
-# info_dict = dict(zip(t_list, b_list))
-# max_info_dict = dict(zip(t_list, b_max_list))
-# for i in index_range:
-#     total_info = 0.0
-#     total_info_using_max = 0.0
-#     for t in t_list[:i+1]:
-#         total_info += info_dict[t]
-#         total_info_using_max += max_info_dict[t]
-#     mse_list.append(1/np.sqrt(total_info))
-#     mse_using_max_info_list.append(1/np.sqrt(total_info_using_max))
-# 
-# ax3 = fig.add_subplot(2, 2, 3)
-# ax3.plot(index_range, mse_list)
-# ax3.plot(index_range, mse_using_max_info_list)
-# ax3.set_xlabel("Number of samples")
-# ax3.set_ylabel("MSE [Hz]")
-# ax3.ticklabel_format(axis="y", style="sci", scilimits=(6, 6), useOffset=False)
+data = []
+for n in range(1, 101):
+    data.append((n,) + find_maxinfo_and_t(n))
+zip_data = list(zip(*data))
+
+ax = fig.add_subplot(2, 2, 1)
+ax.set_title("Maximum Fisher information\n($f=100$MHz)")
+ax.plot(zip_data[0], zip_data[2])
+ax.set_xlabel("$n$")
+ax.set_ylabel("$\mathcal{I}_{\mathrm{max}}(t)$")
+
+ax = fig.add_subplot(2, 2, 2)
+ax.set_title("Argmax_t for the maximum Fisher information\n($f=100$MHz)")
+ax.plot(zip_data[0], zip_data[1])
+ax.set_xlabel("$n$")
+ax.set_ylabel("$t_\mathrm{max}$ [ns]")
 
 plt.tight_layout()
 plt.show()
